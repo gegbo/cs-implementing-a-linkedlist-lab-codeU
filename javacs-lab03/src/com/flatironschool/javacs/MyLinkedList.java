ea23e7cc0059e0c1ceac41d82235c41287f59728
Grace Egbo <grace.egbo1@gmail.com>
2016-04-02 14:23:28 -0500

    Completed lab

--- javacs-lab03/src/com/flatironschool/javacs/MyLinkedList.java
+++ javacs-lab03/src/com/flatironschool/javacs/MyLinkedList.java
@@ -86,6 +86,13 @@ public class MyLinkedList<E> implements List<E> {
 	@Override
 	public void add(int index, E element) {
 		// TODO: fill this in
+		if(index == 0) {
+			head = new Node(element,head);
+		} else {
+			Node node = getNode(index -1);
+			node.next = new Node(element, node.next);
+		}
+		size++;
 	}
 
 	@Override
@@ -147,6 +154,15 @@ public class MyLinkedList<E> implements List<E> {
 	@Override
 	public int indexOf(Object target) {
 		// TODO: fill this in
+		Node node = head; 
+		for(int i = 0; i<size; i++)
+		{
+			if(equals(target, node.cargo))
+			{
+				return i;
+			}
+			node = node.next;
+		}
 		return -1;
 	}
 
@@ -202,7 +218,13 @@ public class MyLinkedList<E> implements List<E> {
 	@Override
 	public boolean remove(Object obj) {
 		// TODO: fill this in
-		return false;
+		int index = indexOf(obj);
+		if(index == -1)
+		{			
+			return false;
+		}
+		remove(index);
+		return true;
 	}
 
 	@Override
